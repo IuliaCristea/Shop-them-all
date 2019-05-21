dd51fefa1cba51a3c24f1d0684c6ed378ac1e210
Ioana <gradinariuioana98@yahoo.ro>
2019-05-22 00:14:20 +0300

    baza de date

--- Shop-them-all/shop/models.py
+++ Shop-them-all/shop/models.py
@@ -3,24 +3,39 @@ from datetime import timezone, datetime
 from django.db import models
 
 # Create your models here.
-from django.forms import forms
 
 
 class Product(models.Model):
     def __str__(self):
         return self.name
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=200)
     price = models.FloatField(max_length=50)
-    picture = models.BinaryField()
-
-
+    picturePath = models.CharField(max_length=300)
+    description = models.CharField(max_length=500)
 
 class User(models.Model):
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=200)
     email = models.EmailField(max_length=200)
     password = models.CharField(max_length=20)
     def __str__(self):
         return self.name
+
+class Buys(models.Model):
+    id_product = models.IntegerField(primary_key=True)
+    id_user = models.IntegerField(primary_key=True)
+
+class Vendor(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+
+class Sells(models.Model):
+    id_product = models.IntegerField(primary_key=True)
+    id_vendor = models.IntegerField(primary_key=True)
+
+
 
 class Smth(models.Model):
     ceva = models.CharField(max_length=2)
