c506ebca6ebe129d56a82f0c7d8ff7523cd027c0
IuliaCristea <iulia.stefania.cristea@gmail.com>
2019-05-28 01:42:36 +0300

    Add Travi CI integration

--- Shop-them-all/shop/tests.py
+++ Shop-them-all/shop/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
 
 # Create your tests here.
+import datetime
+
+from django.test import TestCase
+
+from .models import Smth
+
+
+class SmthModelTests(TestCase):
+
+    def test1(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        smth = Smth();
+        smth.ceva = "wtf?"
+        self.assertIs(smth.ceva == "?wtf?", False)
+
+
+    def test2(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        smth = Smth();
+        smth.ceva = "wtf?"
+        self.assertIs(smth.ceva == "wtf?", True)
