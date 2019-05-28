153507d4a6e04835d3c575ca260bbd9907a5d874
Ioana <gradinariuioana98@yahoo.ro>
2019-05-28 19:44:58 +0300

    Add models

--- Shop-them-all/shop/models.py
+++ Shop-them-all/shop/models.py
@@ -1,42 +1,29 @@
-from datetime import timezone, datetime
-
 from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    def __str__(self):
+        return self.name
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+
+class Shop(models.Model):
+    def __str__(self):
+        return self.name
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
 
 class Product(models.Model):
     def __str__(self):
         return self.name
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     price = models.FloatField(max_length=50)
     picturePath = models.CharField(max_length=300)
+    color = models.CharField(max_length=50, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    size = models.CharField(max_length=5, null=True)
     description = models.CharField(max_length=500)
-
-class Buys(models.Model):
-    class Meta:
-        unique_together = (('id_product', 'id_user'),)
-    id_product = models.IntegerField()
-    id_user = models.IntegerField()
-
-class Vendor(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=50)
-    address = models.CharField(max_length=100)
-
-class Sells(models.Model):
-    class Meta:
-        unique_together = (('id_product', 'id_vendor'),)
-    id_product = models.IntegerField()
-    id_vendor = models.IntegerField()
-
-
-
-class Smth(models.Model):
-    ceva = models.CharField(max_length=2)
-
-class Iulia(models.Model):
-    name = models.CharField(max_length=20)
-    def __str__(self):
-        return self.name
+    id_vendor = models.ForeignKey(Shop, on_delete=models.CASCADE, null=True)
