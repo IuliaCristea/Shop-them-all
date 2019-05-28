3b7ed420bcaf304e85a2b47e457c0f2d565a3407
IuliaCristea <iulia.stefania.cristea@gmail.com>
2019-05-28 12:51:30 +0300

    Redesign to allow view separation

--- Shop-them-all/shop/views/common_views.py
+++ Shop-them-all/shop/views/common_views.py
@@ -1,8 +1,5 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def login(request):
-    return render(request, 'login.html')
-
 def home(request):
     return HttpResponse("Home Page")
