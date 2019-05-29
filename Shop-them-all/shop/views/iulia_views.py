436700f43b0fa8b4b46bec0b45c73b7805f5eb96
Iulia <iulia.stefania.cristea@gmail.com>
2019-05-30 00:31:06 +0200

    Split search functionality

--- Shop-them-all/shop/views/iulia_views.py
+++ Shop-them-all/shop/views/iulia_views.py
@@ -25,17 +25,25 @@ def search(request):
 
     context = {
         'all_shops': all_shops,
-        'shops_map': json.dumps(shops_mapping)
+        'shops_map': json.dumps(shops_mapping),
     }
 
-    searchText = request.GET['search']
+    searchResult = search_internal(request.GET['search'])
+    if(len(searchResult) != 0):
+        context['search_resulted_shop'] = searchResult
+
+    return render(request, 'home.html', context)
+
+def search_internal(searchText):
+
     if len(searchText) > 0:
         searchElements = searchText.split(" ");
         firsFilter = Product.objects.filter(name__contains=searchElements[1].lower())
-        secondFilter = list(set([prod.id_vendor.name for prod in firsFilter if prod.color.lower() == searchElements[0].lower()]))
+        secondFilter = list(
+            set([prod.id_vendor.name for prod in firsFilter if prod.color.lower() == searchElements[0].lower()]))
         if len(secondFilter) == 0:
-           secondFilter = list(set([prod.id_vendor.name for prod in firsFilter]))
+            secondFilter = list(set([prod.id_vendor.name for prod in firsFilter]))
 
-        context['search_resulted_shop'] = json.dumps(secondFilter)
+        return json.dumps(secondFilter)
 
-    return render(request, 'home.html', context)
+    return []
