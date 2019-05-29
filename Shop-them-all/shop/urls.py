43cc55681e39cb72d3acc1316fe7d4099fa6ca10
malinabenegui <malina.benegui@gmail.com>
2019-05-29 19:30:36 +0300

    Updated urls' list to include url for each category of a shop

--- Shop-them-all/shop/urls.py
+++ Shop-them-all/shop/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [url(r'^$', views.home, name='home'),
 			   url(r'^login$', LoginView.as_view(template_name='login.html',  authentication_form = LogInForm, redirect_authenticated_user=True), name="login"),
 			   url(r'^logout', LogoutView.as_view(), name="logout"),
 			   url(r'^product', views.get_prduct_details, name="product"),
+			   path('shop/<str:shop_name>/<str:categ>', views.get_prod_by_categ),
 			   path('shop/<str:shop_name>', views.get_shop_list),
 			   ]
