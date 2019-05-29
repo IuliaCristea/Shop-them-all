94a8a5336eee0a269615de3f468e9a662fad2049
ramonaistoc <RAMONA-CRISTINA.ISTOC@my.fmi.unibuc.ro>
2019-05-30 02:33:26 +0300

    teste

--- Shop-them-all/shop/tests.py
+++ Shop-them-all/shop/tests.py
@@ -3,9 +3,11 @@ from django.test import TestCase
 # Create your tests here.
 import datetime
 
-from django.test import TestCase
-
-
+from django.test import TestCase, RequestFactory
+from .models import Category
+from .models import Product
+from .views import iulia_views
+from django import views
 
 class SmthModelTests(TestCase):
     pass
@@ -27,3 +29,37 @@ class SmthModelTests(TestCase):
     #     smth = Smth();
     #     smth.ceva = "wtf?"
     #     self.assertIs(smth.ceva == "wtf?", True)
+
+class Test(TestCase):
+    def testSkirt(self):
+        self.factory = RequestFactory()
+        obj = Product.objects.filter(name="Skirt", color="Red")
+        #obj.save()
+
+        request = self.factory.get('/search?search=red+skirt')
+        response = iulia_views.search(request)
+        #print(response.context[-1]['search_resulted_shop'])
+        self.assertEqual(response.status_code, 200)
+        #self.assertIn(len(result), response.context[-1]['search_resulted_shop'])
+
+    def testShirt(self):
+        self.factory = RequestFactory()
+        obj = Product.objects.filter(name="Shirt", color="Blue")
+        # obj.save()
+
+        request = self.factory.get('/search?search=shirt+blue')
+        response = iulia_views.search(request)
+        # print(response.context[-1]['search_resulted_shop'])
+        self.assertEqual(response.status_code, 200)
+        # self.assertIn(len(result), response.context[-1]['search_resulted_shop'])
+
+    def testPants(self):
+        self.factory = RequestFactory()
+        obj = Product.objects.filter(name="Pants", color="Blue")
+        # obj.save()
+
+        request = self.factory.get('/search?search=pants+blue')
+        response = iulia_views.search(request)
+        # print(response.context[-1]['search_resulted_shop'])
+        self.assertEqual(response.status_code, 200)
+        # self.assertIn(len(result), response.context[-1]['search_resulted_shop'])
