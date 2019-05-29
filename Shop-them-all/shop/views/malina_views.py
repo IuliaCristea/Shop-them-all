8c5a9c7735838e60196d184ef0748916e390d0b5
malinabenegui <malina.benegui@gmail.com>
2019-05-29 12:35:27 +0300

    Get products by category

--- Shop-them-all/shop/views/malina_views.py
+++ Shop-them-all/shop/views/malina_views.py
@@ -17,3 +17,19 @@ def get_shop_list(request, shop_name):
         'all_categories': all_categories
     }
     return render(request,'shop_details.html', context)
+
+
+def get_prod_by_categ(request, shop_name, categ):
+
+    shop = Shop.objects.get(name=shop_name)
+    all_prods = Product.objects.filter(id_vendor=shop)
+    all_categories = [prod.category for prod in all_prods]
+    all_categories = list(set(all_categories))
+    category = Category.objects.get(name = categ)
+    all_prods = Product.objects.filter(id_vendor=shop, category=category)
+    #all_prods = [prod for prod in all_prods if prod.category == category]
+    context = {
+        'all_prods': all_prods,
+        'all_categories': all_categories
+    }
+    return render(request,'shop_details.html', context)
