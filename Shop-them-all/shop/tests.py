b80ebd4438a798e686c0586ca40398683c75dbb5
IuliaCristea <iulia.stefania.cristea@gmail.com>
2019-05-30 03:31:49 +0300

    fix conflict

--- Shop-them-all/shop/tests.py
+++ Shop-them-all/shop/tests.py
@@ -5,25 +5,30 @@ import datetime
 
 from django.test import TestCase
 
+from django.contrib.auth.models import AnonymousUser, User
+from django.test import RequestFactory, TestCase
 
+class SimpleTest(TestCase):
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username='jacob', email='jacob@…', password='top_secret')
 
-class SmthModelTests(TestCase):
-    pass
-    # def test1(self):
-    #     """
-    #     was_published_recently() returns False for questions whose pub_date
-    #     is in the future.
-    #     """
-    #     smth = Smth();
-    #     smth.ceva = "wtf?"
-    #     self.assertIs(smth.ceva == "?wtf?", False)
-    #
-    #
-    # def test2(self):
-    #     """
-    #     was_published_recently() returns False for questions whose pub_date
-    #     is in the future.
-    #     """
-    #     smth = Smth();
-    #     smth.ceva = "wtf?"
-    #     self.assertIs(smth.ceva == "wtf?", True)
+    def test_details(self):
+        # Create an instance of a GET request.
+        request = self.factory.get('/customer/details')
+
+        # Recall that middleware are not supported. You can simulate a
+        # logged-in user by setting request.user manually.
+        request.user = self.user
+
+        # Or you can simulate an anonymous user by setting request.user to
+        # an AnonymousUser instance.
+        request.user = AnonymousUser()
+
+        # Test my_view() as if it were deployed at /customer/details
+        response = my_view(request)
+        # Use this syntax for class-based views.
+        response = MyView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
--- Shop-them-all/shop/views/ioana_views.py
+++ Shop-them-all/shop/views/ioana_views.py
@@ -4,10 +4,11 @@ from ..models import Shop
 from ..models import Product
 
 def cart(request):
-    all_shops = Shop.objects.all()
-    all_selected_prod = Product.objects.all()
+
+    ids = [request.POST['cart_list'].split(',')]
+    all_selected_prod = Product.objects.filter(pk__in=ids[0])
     context = {
-        'all_shops': all_shops,
         'all_selected_prods': all_selected_prod,
     }
     return render(request, 'cart_page.html',context)
+
